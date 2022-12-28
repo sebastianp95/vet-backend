9bdd7ea5222bba3a80fef7e6cf6a73d48f62df30
J. Sebastian Perez <sebastian_perez95@hotmail.com>
2022-12-28 14:53:06 -0500

    ST6

---
+++ src/main/java/com/dev/vetbackend/VetBackendApplication.java
@@ -0,0 +1,15 @@
+package com.dev.vetbackend;
+
+import com.dev.vetbackend.entities.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetBackendApplication.class, args);
+
+		 	}
+
+}
