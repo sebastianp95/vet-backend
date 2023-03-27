d332adc9f3a14873d317a13985e533cbe0638cbd
sebastianp95 <sebastian_perez95@hotmail.com>
2023-03-27 17:27:02 -0400

    St35

--- src/main/java/com/dev/vetbackend/controller/PaymentController.java
+++ src/main/java/com/dev/vetbackend/controller/PaymentController.java
@@ -4,8 +4,11 @@ import com.dev.vetbackend.entity.PaymentRequest;
 import com.dev.vetbackend.payment.StripePayment;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +29,16 @@ public class PaymentController {
             return new ResponseEntity<>(charge.getId(), HttpStatus.OK);
         } catch (StripeException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/create-payment-intent")
+    public ResponseEntity<String> createPaymentIntent() {
+        try {
+            String clientSecret = stripePayment.createPaymentIntent(100, "usd");
+            return ResponseEntity.ok(clientSecret);
+        } catch (StripeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error creating PaymentIntent: " + e.getMessage());
         }
     }
 }
--- src/main/java/com/dev/vetbackend/payment/StripePayment.java
+++ src/main/java/com/dev/vetbackend/payment/StripePayment.java
@@ -3,6 +3,7 @@ package com.dev.vetbackend.payment;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,9 @@ public class StripePayment {
     @Value("${stripe.secret}")
     private String API_SECRET_KEY;
 
+    @Value("${stripe.secret.test}")
+    private String API_SECRET_KEY_TEST;
+
     public Charge chargeCreditCard(String token, int amount, String currency) throws StripeException {
         Stripe.apiKey = API_SECRET_KEY;
 
@@ -25,6 +29,20 @@ public class StripePayment {
         chargeParams.put("source", token);
 
         return Charge.create(chargeParams);
+    }
+
+    public  String createPaymentIntent(int amount, String currency) throws StripeException {
+        Stripe.apiKey = API_SECRET_KEY;
+        System.out.println(API_SECRET_KEY);
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", amount);
+        params.put("currency", currency);
+        // Add any additional parameters here, such as customer or metadata
+
+        PaymentIntent paymentIntent = PaymentIntent.create(params);
+
+        return paymentIntent.getClientSecret();
     }
 
 }
