f6b2591db2b5b9ebf6c585bb178b9ba7c8fbb6ea
sebastianp95 <sebastian_perez95@hotmail.com>
2023-04-30 13:59:36 -0400

    Create default pet

--- src/main/java/com/dev/vetbackend/security/UserDetailServiceImpl.java
+++ src/main/java/com/dev/vetbackend/security/UserDetailServiceImpl.java
@@ -1,7 +1,11 @@
 package com.dev.vetbackend.security;
 
 
+import com.dev.vetbackend.entity.MedicalHistory;
+import com.dev.vetbackend.entity.Pet;
 import com.dev.vetbackend.entity.User;
+import com.dev.vetbackend.repository.MedicalHistoryRepository;
+import com.dev.vetbackend.repository.PetRepository;
 import com.dev.vetbackend.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,21 +21,31 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
 
     private UserRepository userRepository;
+    private PetRepository petRepository;
+    private MedicalHistoryRepository medicalHistoryRepository;
     private PasswordEncoder passwordEncoder;
     @Value("${stripe.plan.basic}")
     private String BASIC_PLAN_PRODUCT_ID;
 
     @Autowired
-    public UserDetailServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserDetailServiceImpl(
+            UserRepository userRepository,
+            PetRepository petRepository,
+            MedicalHistoryRepository medicalHistoryRepository,
+            PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.petRepository = petRepository;
+        this.medicalHistoryRepository = medicalHistoryRepository;
         this.passwordEncoder = passwordEncoder;
     }
+
 
     @Override
     public UserDetails loadUserByUsername(String email) {
@@ -71,8 +85,11 @@ public class UserDetailServiceImpl implements UserDetailsService {
         user.setEmail(email);
         user.setName(name);
         user.setPassword(passwordEncoder.encode(password));
-        // save the new user to the repository
+        // save the new user , first pet, and first medical history to the database
         updateUserSubscription(user, BASIC_PLAN_PRODUCT_ID, "basic", "active");
+        Pet created = createFirstPet(user);
+        createFirstMedicalHistory(created, user);
+
 
     }
 
@@ -84,4 +101,34 @@ public class UserDetailServiceImpl implements UserDetailsService {
         // Save the updated user to the database
         userRepository.save(user);
     }
+
+    public Pet createFirstPet(User user) {
+        Pet newPet = new Pet();
+        newPet.setAge("2");
+        newPet.setBreed("Akita");
+        newPet.setImageSrc("https://res.cloudinary.com/dgghss1d9/image/upload/v1680189604/vetpics/xln8gv3vjo3yczug8l56.jpg");
+        newPet.setName("Otto");
+        newPet.setUser(user);
+        newPet.setWeight(22);
+        newPet.setOwnerId(4046235978L);
+
+        return petRepository.save(newPet);
+    }
+
+
+    public void createFirstMedicalHistory(Pet pet, User user) {
+        MedicalHistory medicalHistory = new MedicalHistory();
+        medicalHistory.setDateOfVisit("2023-04-30");
+        medicalHistory.setReasonForVisit("Routine checkup");
+        medicalHistory.setCurrentMedications(List.of("Heartworm prevention", "Flea and tick prevention"));
+        medicalHistory.setKnownAllergies(List.of("None"));
+        medicalHistory.setPreviousSurgeriesOrIllness(List.of("None"));
+        medicalHistory.setVeterinarianObservations("Pet is in good health");
+        medicalHistory.setTreatmentPlanAndRecommendations("Continue with current medications and schedule follow-up appointment in 6 months");
+        medicalHistory.setUser(user);
+        medicalHistory.setPet(pet);
+
+        medicalHistoryRepository.save(medicalHistory);
+    }
+
 }
