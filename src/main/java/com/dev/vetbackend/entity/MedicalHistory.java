dc1eeee556a6256733bd8ab731f65a8beafaf94b
J. Sebastian Perez <sebastian_perez95@hotmail.com>
2024-09-26 16:42:41 -0400

    new attributes

--- src/main/java/com/dev/vetbackend/entity/MedicalHistory.java
+++ src/main/java/com/dev/vetbackend/entity/MedicalHistory.java
@@ -3,14 +3,7 @@ package com.dev.vetbackend.entity;
 import com.dev.vetbackend.utils.StringListToJsonConverter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
@@ -24,28 +17,36 @@ public class MedicalHistory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String dateOfVisit;
-    @Column(length = 800)
+    private String conductedBy;
+    @Column(length = 500)
     private String reasonForVisit;
-    @Convert(converter = StringListToJsonConverter.class)
-    private List<String> currentMedications;
-
-    @Convert(converter = StringListToJsonConverter.class)
-    private List<String> knownAllergies;
-
-    @Convert(converter = StringListToJsonConverter.class)
-    private List<String> previousSurgeriesOrIllness;
-    @Column(length = 800)
-    private String veterinarianObservations;
-    @Column(length = 800)
-    private String treatmentPlanAndRecommendations;
+    @Column(length = 500)
+    private String anamnesis;
     private int weight;
     private int age;
+    private double temperature;
+    private int heartRate;
+    @Column(length = 500)
+    private String generalCondition;
+    @Column(length = 500)
+    private String skinAndCoat;
+    @Column(length = 500)
+    private String respiratorySystem;
+    @Column(length = 500)
+    private String cardiovascularSystem;
+    @Column(length = 500)
+    private String musculoskeletalSystem;
+    @Column(length = 500)
+    private String genitourinary;
+    @Column(length = 500)
+    private String veterinarianObservations;
+    @Column(length = 500)
+    private String treatmentPlanAndRecommendations;
     @JsonIgnore
     @ManyToOne(optional = false)
     private User user;
     @ManyToOne(optional = false)
     @JoinColumn(name = "pet_id")
     private Pet pet;
-
 }
 
